c348a3aadc39f5a0c58092d6b48a6d26a1ba25ec
Angeluss2023 <121591004+Angeluss2023@users.noreply.github.com>
2023-05-03 09:00:34 -0500

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,53 @@
+import 'zone.js/dist/zone';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'my-app',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './main.html',
+  styleUrls: ['./main.css'],
+})
+export class App {
+  name = 'Bryam';
+  cargo = 'Docente';
+  a = 0;
+  b = 0;
+  c = 0;
+  contador = 0;
+
+  sumar() {
+    this.contador++;
+    this.c = Number(this.a) + Number(this.b);
+  }
+
+  restar() {
+    this.contador++;
+    this.c = this.a - this.b;
+  }
+
+  multiplicar() {
+    this.contador++;
+    this.c = this.a * this.b;
+  }
+
+  dividir() {
+    this.contador++;
+    if (this.b !== 0) {
+      this.c = this.a / this.b;
+    } else {
+      this.c = NaN;
+    }
+  }
+
+  calcular() {
+    console.log('Hola UPS', this.a);
+    this.contador = this.contador + 1;
+    this.c = 0 + this.a + eval('' + this.b);
+  }
+}
+
+bootstrapApplication(App);

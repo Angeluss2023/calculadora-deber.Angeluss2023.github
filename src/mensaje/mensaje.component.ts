0744de9edb26f9405dd89225b8f6127c9c9f1a68
Angeluss2023 <121591004+Angeluss2023@users.noreply.github.com>
2023-05-04 08:06:38 -0500

    2 files modified, 6 added

---
+++ src/mensaje/mensaje.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mensaje',
+  templateUrl: './mensaje.component.html',
+  standalone: true,
+  styleUrls: ['./mensaje.component.css'],
+})
+export class MensajeComponent implements OnInit {
+  constructor() {}
+
+  @Input() name = 'bryam';
+
+  ngOnInit() {}
+}
